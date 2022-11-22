a38c7d00aa848158044e4f5131453fa71720579e
hasan <mdhasanhk.98@gmail.com>
2022-11-22 13:02:26 +0600

    all done overlay

--- starter/script.js
+++ starter/script.js
@@ -1,21 +1,21 @@
 'use strict';
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const showModal = document.querySelectorAll('.show-modal');
 const closeModal = document.querySelector('.close-modal');
 
-for (let i = 0; i < showModal.length; i++) {
-  showModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+const openModals = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModals = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-  closeModal.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
-  overlay.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModals);
+  closeModal.addEventListener('click', closeModals);
+  overlay.addEventListener('click', closeModals);
 }
